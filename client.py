4928ea2eace52bb0cea1b7e660bde15afb5f449c
Jaya Sai Karanam <65283616+JayasaiK@users.noreply.github.com>
2024-07-22 11:38:12 -0500

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,8 @@ async def interact():
     uri = "ws://localhost:8000/start_task"
     async with websockets.connect(uri) as websocket:
         # Send initial user input (e.g., CUID)
-        user_input = {"user_input": "ADXXXXX"}  # Example CUID
+        user_input = {"type": "cuid", "data": "ADXXXXX"}  # Example CUID
+        append_value(f"To backend: {user_input}")
         await websocket.send(json.dumps(user_input))
  
         while True:
@@ -15,11 +16,22 @@ async def interact():
             #print("Server:", response)
             response = json.loads(response)
             # Check if the server message asks for further input or confirmation
+            append_value(f"from backend: {response}")
+            
             if 'data' in response:
                 print(response['data'])
             print(response['message'])
             if response['type'] == 'input':
                 user_response = input("Enter response: ")
-                await websocket.send(user_response)
+                to_send = {"type": "message", "data": user_response}
+                await websocket.send(json.dumps(to_send))
+            
+            append_value(f"To backend: {to_send}")
+            
+def append_value(new_value):
+    """Appends a new value to the text file."""
+    file_path = "tracker.txt"
+    with open(file_path, 'a') as file:
+        file.write(f"{new_value}\n")
 
 asyncio.run(interact())
